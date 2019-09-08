636c0b7f0153fe80ad6f4c42b2be4375fdcc159e
Stanislav Dolgachov <s.dolgachov@gmail.com>
2019-09-08 21:37:20 +0300

    Добавил карусель вместе с решением

--- src/js/script.js
+++ src/js/script.js
@@ -31,6 +31,9 @@ function run() {
 
     const menuWrapper = document.querySelector('.main-menu');
     const menu = new Menu(menuWrapper);
+
+    const carouselWrapper = document.querySelector('.carousel');
+    const carousel = new Carousel(carouselWrapper);
 }
 
 run();
