12e2fd32cab2f81e9c1c1a03f444d97a928bc478
vshhesyuk <vshhesyuk@mail.ru>
2019-09-19 20:52:14 +0700

    Добавлена компонента login-form

--- src/js/login-form.js
+++ src/js/login-form.js
@@ -77,7 +77,120 @@
 
 class LoginForm {
     constructor(element) {
-        // Ваш код
+        this.element = element;
+
+        this.render(element);
+    }
+
+    render(element) {
+        let layout = 
+        `<div class="row justify-content-center">
+            <div class="col-md-6">
+                <form name="loginForm">
+                    <div class="alert alert-danger" role="alert" style="display: none;">
+                        Passwords should be the same!
+                    </div>
+                    <div class="jumbotron jumbotron-light jumbotron-form">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email</label>
+                            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
+                        </div>
+        
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+                        </div>
+        
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password Check</label>
+                            <input type="password" name="passwordCheck" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+                        </div>
+        
+                        <div class="form-check mb-4">
+                            <input type="checkbox"  class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+        
+                        <button type="submit" class="btn btn-primary btn-lg by-now-button">Buy now</button>
+                    </div>
+                </form>
+                <div class="h4 text-center font-weight-normal text-uppercase my-5 d-none d-lg-block">or</div>
+                <div class="socials mb-5">
+                    <a href="#" class="btn btn-primary btn-social -fb">
+                        <img src="assets/icons/icon-facebook.svg" alt="">
+                        Facebook
+                    </a>
+                    <a href="#" class="btn btn-primary btn-social -vk">
+                        <img src="assets/icons/icon-vk.svg" alt="">
+                        Vkontakte
+                    </a>
+                    <a href="#" class="btn btn-primary btn-social -twitter">
+                        <img src="assets/icons/icon-twitter.svg" alt="">
+                        Twitter
+                    </a>
+                </div>
+            </div>
+        </div>`;
+
+        this.element.innerHTML = layout;
+
+        const registerUrl="https://httpbin.org/post";
+
+        let registerForm = document.forms.loginForm;
+        let emailField = registerForm.email;
+        let passwordField = registerForm.password;
+        let passwordCheckField = registerForm.passwordCheck;
+        let formCheckInput = registerForm[3];
+        
+        
+        passwordCheckField.addEventListener('blur', () => {
+
+            let password = passwordField.value;
+            let passwordCheck = passwordCheckField.value;
+
+            if (password !== passwordCheck) {
+                let alertDanger = document.querySelector('.alert-danger');
+                alertDanger.style.display = 'block';
+            }
+
+        });
+
+        passwordField.addEventListener('focus', () => {
+            let alertDanger = document.querySelector('.alert-danger');
+            alertDanger.style.display = 'none';
+        });
+
+        passwordCheckField.addEventListener('focus', () => {
+            let alertDanger = document.querySelector('.alert-danger');
+            alertDanger.style.display = 'none';
+        });
+
+        registerForm.addEventListener('submit', (event) => {
+
+            event.preventDefault();
+
+            let email = emailField.value;
+            let password = passwordField.value;
+            let check = formCheckInput.checked;
+
+            let visitor = {
+                email: email,
+                password: password,
+                check: check
+            };
+
+            fetch(registerUrl, {
+                method: 'POST',
+                body: JSON.stringify(visitor)
+              })
+              .then((response) => response.json())
+              .then((result) => console.log(result));
+
+            
+          registerForm.reset();
+               
+        });
+        
     }
 }
 
